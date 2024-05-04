3fa00ad98e57a4cac267e45469ae0e2ebe84e2bc
DicksonPal <dickson@metromart.com>
2024-05-04 22:02:32 +0800

    Wrapped with forwardref

--- src/components/Card/index.tsx
+++ src/components/Card/index.tsx
@@ -1,26 +1,25 @@
 import cn from 'classnames'
+import { forwardRef } from 'react'
 import { Types } from './types'
 
-const Card = ({
-  className,
-  children,
-  loading,
-  loader,
-  hoverable,
-  ...props
-}: Types) => {
-  const cardProps = {
-    className: cn(
-      'w-full transition-colors border backdrop-blur-2xl min-h-[96px] rounded-lg p-4',
-      hoverable
-        ? 'border-transparent hover:border-neutral-800 hover:bg-zinc-800/30'
-        : 'bg-zinc-800/30 border-neutral-800',
-      className
-    ),
-    ...props,
-  }
+const Card = forwardRef<HTMLDivElement, Types>(
+  ({ className, children, loading, loader, hoverable, ...props }, ref) => {
+    const cardProps = {
+      className: cn(
+        'w-full transition-colors border backdrop-blur-2xl min-h-[96px] rounded-lg p-4',
+        hoverable
+          ? 'border-transparent hover:border-neutral-800 hover:bg-zinc-800/30'
+          : 'bg-zinc-800/30 border-neutral-800',
+        className
+      ),
+      ...props,
+      ref,
+    }
 
-  return <div {...cardProps}>{loading ? loader : children}</div>
-}
+    return <div {...cardProps}>{loading ? loader : children}</div>
+  }
+)
+
+Card.displayName = 'Card'
 
 export default Card
